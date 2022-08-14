86e9dd4ebcfcf9b4ec526386c86698d055192ff9
Diego Rivera <93798677+Diegorivera1110@users.noreply.github.com>
2022-08-14 12:46:21 -0700

    deploying

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
+mongoose.connect(`mongodb+srv://diegorivera1110:softgrass123@cluster0.ojik8ln.mongodb.net/googlebooks`, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
